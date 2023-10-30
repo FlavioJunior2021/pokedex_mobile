73eb5ddf3dd1856c7007c7529182544fe10b3701
Flávio Oliveira Silva Júnior <89926456+FlavioJunior2021@users.noreply.github.com>
2023-10-30 12:55:22 -0300

    App estructure

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,11 @@
+import { TouchableOpacity, Text } from "react-native";
+
+export function TouchableOpacityButton() {
+	return (
+		<TouchableOpacity>
+			<Text>
+				Botão
+			</Text>
+		</TouchableOpacity>
+	);
+}
